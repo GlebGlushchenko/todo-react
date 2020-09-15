80aed441a103a8557ce8e5d088452f8847264f93
GlebGlushchenko <darkvee1n@ya.ru>
2020-09-15 18:03:32 +0300

    Updates

---
+++ precache-manifest.ff773cb85786bcee163656782b3054c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c18f8b4c3c53be5d9f05edf1cbbb48",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "4b982678e7566060f481",
+    "url": "/todo-react/static/css/main.d9899116.chunk.css"
+  },
+  {
+    "revision": "4c68b8e27c2e2be071f8",
+    "url": "/todo-react/static/js/2.3024401f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-react/static/js/2.3024401f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b982678e7566060f481",
+    "url": "/todo-react/static/js/main.eb2d25c3.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
